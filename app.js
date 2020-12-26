a4c836d1f3d538a2d365af745f8042e8c82f512b
Duhan Renteria <derh7280@hotmail.com>
2020-12-26 01:45:52 -0500

    Implementando newrelic

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();//libreria para trabajar con variables de ambiente
-// require('newrelic');
+require('newrelic');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
